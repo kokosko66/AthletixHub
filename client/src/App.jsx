c8d42edafb3ea1e1f579655bb047b6cbeee2a0a4
Rosen Raykov <rg.raikov.eu@gmail.com>
2025-02-25 10:36:18 +0200

    Created LoginPage and styled it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <LoginPage/>
     </div>
   )
 }
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,45 @@
+import dumbbell from '../assets/dumbbell.png';
+import '../styles/LoginPage.css';
+
+export default function Loginpage() {
+    return (
+        <div className="container">
+            <div className="top-container">
+                <h1><img src={dumbbell} alt="icon" className='dumbbell-icon' />AthletixHub</h1>
+            </div>
+            <div className='main-container'>
+                <div className="register-form">
+                    <h2>Join AthletixHub</h2>
+                    <form>
+                        <label>Name</label>
+                        <br/>
+
+                        <input type="text" placeholder="Enter your name"/>
+                        <br/>
+
+                        <label>Email</label>
+
+                        <br />
+                        <input type="email" placeholder="Enter your email"/>
+
+                        <br />
+                        <label>Phone</label>
+
+                        <br />
+                        <input type="tel" placeholder="Enter your phone number"/>
+
+                        <br />
+                        <label>Password</label>
+
+                        <br />
+                        <input type="password" placeholder="Enter your password"/>
+
+                        <br />
+                        
+                        <button>Create Profile</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
