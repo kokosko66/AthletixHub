cf1d5bf9e43f56ff36f5ec8770ee8ad456c2638e
Rosen Raykov <rg.raikov.eu@gmail.com>
2025-02-25 12:16:52 +0200

    Fixed register page, implemented an option to login

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 import dumbbell from '../assets/dumbbell.png';
-import '../styles/LoginPage.css';
+import '../styles/RegisterPage.css';
 
 export default function Loginpage() {
+    const [role, setRole]  = useState('trainee');
+
     return (
         <div className="container">
             <div className="top-container">
@@ -10,33 +14,43 @@ export default function Loginpage() {
             <div className='main-container'>
                 <div className="register-form">
                     <h2>Join AthletixHub</h2>
+                    <div className={`role-switch ${role === 'trainer' ? 'trainer-active' : ''}`}>
+                        <button 
+                            onClick={() => { setRole('trainee'); }} 
+                            className='role-button'
+                        >
+                            Trainee
+                        </button>
+                        <button 
+                            onClick={() => { setRole('trainer'); }} 
+                            className='role-button'
+                        >
+                            Trainer
+                        </button>
+                        <div className="switch-indicator"></div>
+                    </div>
+
                     <form>
                         <label>Name</label>
                         <br/>
-
                         <input type="text" placeholder="Enter your name"/>
                         <br/>
-
                         <label>Email</label>
-
                         <br />
                         <input type="email" placeholder="Enter your email"/>
-
                         <br />
                         <label>Phone</label>
-
                         <br />
                         <input type="tel" placeholder="Enter your phone number"/>
-
                         <br />
                         <label>Password</label>
-
                         <br />
                         <input type="password" placeholder="Enter your password"/>
-
                         <br />
-
-                        <button>Create Profile</button>
+                        <button className='profile-button'>Create Profile</button>
+                        <br />
+                        <br />
+                        <p className='if-login'>Already have an account? <a href='/login' className='login-link'>Login</a></p>
                     </form>
                 </div>
             </div>
