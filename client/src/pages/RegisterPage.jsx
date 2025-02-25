2ddc1d22d2f352e8401e52c1fcd42e1c9b3d0eb8
Rosen <75417631+kokosko66@users.noreply.github.com>
2025-02-25 13:33:53 +0200

    Login and register page created
    * Created the controller, queries and routes for the exercises table. Also removed the $ from the queries and opted for ? placeholders
    
    * Implemented routes, queries and controller for the foods table
    
    * Implemented routes, queries and controller for the MealPlans table.
    
    * Created routes, queries and controller for the userWorkouts table
    
    * Implemented routes, queries and controller for the userMealPlans table
    
    * Implemented routes, queries and controller for the WorkoutExercisesTable
    
    * Implemented routes, queries and controller for the mealPlanFoodsTable
    
    * Initial commit for the frontend
    
    * Created the HomePage with its style. Without connection to the back-end, only dummy data
    
    * Created LoginPage and styled it
    
    * Same as above, forgot to add a few changes
    
    * Fixed register page, implemented an option to login
    
    * Created a login page, also implemented router for easy navigation between pages

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import dumbbell from '../assets/dumbbell.png';
+import '../styles/RegisterPage.css';
+
+export default function Loginpage() {
+    const [role, setRole]  = useState('trainee');
+
+    return (
+        <div className="container">
+            <div className="top-container">
+                <h1><img src={dumbbell} alt="icon" className='dumbbell-icon' />AthletixHub</h1>
+            </div>
+            <div className='main-container'>
+                <div className="register-form">
+                    <h2>Join AthletixHub</h2>
+                    <div className={`role-switch ${role === 'trainer' ? 'trainer-active' : ''}`}>
+                        <button 
+                            onClick={() => { setRole('trainee'); }} 
+                            className='role-button'
+                        >
+                            Trainee
+                        </button>
+                        <button 
+                            onClick={() => { setRole('trainer'); }} 
+                            className='role-button'
+                        >
+                            Trainer
+                        </button>
+                        <div className="switch-indicator"></div>
+                    </div>
+
+                    <form>
+                        <label>Name</label>
+                        <br/>
+                        <input type="text" placeholder="Enter your name"/>
+                        <br/>
+                        <label>Email</label>
+                        <br />
+                        <input type="email" placeholder="Enter your email"/>
+                        <br />
+                        <label>Phone</label>
+                        <br />
+                        <input type="tel" placeholder="Enter your phone number"/>
+                        <br />
+                        <label>Password</label>
+                        <br />
+                        <input type="password" placeholder="Enter your password"/>
+                        <br />
+                        <button className='profile-button'>Create Profile</button>
+                        <br />
+                        <br />
+                        <p className='if-login'>Already have an account? <Link className='login-link' to='/login'>Login</Link></p>
+                    </form>
+                </div>
+            </div>
+            <div className="register-footer">
+                <p>©2025 AthletixHub. All rights reserved.</p>
+            </div>
+        </div>
+    );
+}
