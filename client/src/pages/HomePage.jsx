916e1b597a788a238aae7c3a23af0af4a4247262
Rosen Raykov <rg.raikov.eu@gmail.com>
2025-02-27 12:23:46 +0200

    Implemented the navigation bar as component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,22 +1,13 @@
 import '../styles/HomePage.css';
-import dumbbell from '../assets/dumbbell.png';
-import { Link } from 'react-router-dom';
+import NavBar from './NavBar';
 
 export default function HomePage() {
     return(
-        <div className='home-container'>
-            <div className="top-bar">
-                <h1><img src={dumbbell} alt="icon" className='dumbbell-icon' />AthletixHub</h1>
-                <nav>
-                    <ul>
-                        <li><Link className='link-design'>Trainers</Link></li>
-                        <li><Link className='link-design'>Workouts</Link></li>
-                        <li><Link className='link-design'>Meals</Link></li>
-                        <li><Link className='link-design'>Profile</Link></li>
-                    </ul>
-                </nav>
-            </div>
-            
+        <div className='home-page'>
+            <NavBar/>
+            <section>
+                <h1>Test</h1>
+            </section>
         </div>
     );
 }
