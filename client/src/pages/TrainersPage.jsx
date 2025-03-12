c07a92eca371d11696fbddfe07348ae12bce2909
Rosen Raykov <rg.raikov.eu@gmail.com>
2025-03-12 11:55:15 +0200

    Created dialog window for requesting workouts from the trainers

--- client/src/pages/TrainersPage.jsx
+++ client/src/pages/TrainersPage.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react';
-import '../styles/TrainersPage.css';
-import NavBar from '../components/NavBar';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import "../styles/TrainersPage.css";
+import NavBar from "../components/NavBar";
+import axios from "axios";
+import Dialog from "../components/Dialog";
+import "../styles/Dialog.css";
 
 export default function TrainersPage() {
     const [trainers, setTrainers] = useState([]);
+    const [selectedTrainer, setSelectedTrainer] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/users/role/trainer')
+        axios
+            .get("http://localhost:3000/api/users/role/trainer")
             .then((response) => {
                 setTrainers(response.data);
             })
@@ -16,17 +19,29 @@ export default function TrainersPage() {
                 console.log(error);
             });
     }, []);
+
     return (
-        <div className='trainers-page'>
+        <div className="trainers-page">
             <NavBar />
             <section>
-                {
-                    trainers.map((trainer) => (
-                        <ul className='trainers-list' key={trainer.id}>
-                            <li>{trainer.name} <span>description</span> <Link className='request-workout'>Request Workout</Link></li>
-                        </ul>
-                    ))
-                }
+                {trainers.map((trainer) => (
+                    <ul className="trainers-list" key={trainer.id}>
+                        <li>
+                            {trainer.name} <span>description</span>{" "}
+                            <button 
+                                onClick={() => setSelectedTrainer(trainer)} 
+                                className="request-workout"
+                            >
+                                Request Workout
+                            </button>
+                        </li>
+                    </ul>
+                ))}
+
+                <Dialog isOpen={!!selectedTrainer} onClose={() => setSelectedTrainer(null)}>
+                    <h2>Request Workout with {selectedTrainer?.name}</h2>
+                    <p>Here you can request a workout session.</p>
+                </Dialog>
             </section>
         </div>
     );
